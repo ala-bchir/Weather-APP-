ff70970793a849fe3925c6a359cef0d60c742a3f
ala-bchir <ala.bchir@yahoo.fr>
2022-03-15 17:18:12 +0100

    Dispaly city name and weather with socket.io

--- server.js
+++ server.js
@@ -1,66 +1,100 @@
-const express = require('express')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server,{cors:{origin:"*"}})
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server)
+let request = require('request');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
-
-
-server.listen(3001,() =>{
-    console.log("Server running");
-});
-
-io.on('connection',(socket)=>{
-    console.log("user connected"+socket.id);    
+app.get("/", function(req,res){
+    res.sendFile(__dirname + '/index.html')
 })
 
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
 
-
-var request = require('request');
-// table with different names of cities.
-var tab = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
 // sleep fct allows to send a request every "n" milliseconds
 function sleep(n) {
     const start = Date.now();
     while (Date.now() - start < n);
-} 
+}
 
-for (let i=0;i<5;i++){
-    var options = {
+let cities = ["casablanca", "tunis", "paris"];
+
+
+
+// const Id = setInterval(()=>{
+
+
+    
+
+//       if(i == 4) clearInterval(Id);
+
+//       i++
+// },1000)
+
+for(let city of cities){
+
+    const options = {
         method: 'GET',
         url: 'https://community-open-weather-map.p.rapidapi.com/find',
         qs: {
-        q: tab[i], // city name taken from the tab 
-        cnt: '1',
-        mode: 'null',
-        lon: '0',
-        type: 'link, accurate',
-        lat: '0',
-        units: 'imperial, metric'
+          q: city,
+          cnt: '1',
+          mode: 'null',
+          lon: '0',
+          type: 'link, accurate',
+          lat: '0',
+          units: 'imperial, metric'
         },
         headers: {
-        'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
-        'x-rapidapi-key': '8a345fcc6amshe00c0e37b584ee8p13a2e9jsn8eafe56699c5',
-        useQueryString: true
+          'x-rapidapi-host': 'community-open-weather-map.p.rapidapi.com',
+          'x-rapidapi-key': '8a345fcc6amshe00c0e37b584ee8p13a2e9jsn8eafe56699c5',
+          useQueryString: true
         }
-    };
-
-
-
-    request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        var jo = JSON.parse(body); // convert body to a json object
-        var city_name = jo.list[0].name; // extract city name
-        var weather_status = jo.list[0].weather[0].main; // extract weather
-        console.log(city_name,weather_status);
-        sleep(500)
-       
-        
-        
-    });
+      };
+      
+      
+      
+      
+      
+      request(options, function (error, response, body) {
+          if (error) throw new Error(error);
+          let jo = JSON.parse(body); // convert body to a json object
+          
+            
     
+          let cityName = jo.list[0].name;
+          let weather_status = jo.list[0].weather[0].main;
+          console.log(cityName,weather_status);
+          sleep(3000)
+      
+          
+          io.on('connection', (socket)=>{
+              console.log('connected')
+              socket.on('disconnect', ()=>{
+                  console.log('disconnected')
+                  
+              })
+              io.emit('city', cityName)
+              io.emit('status', weather_status)
+              
+              
+          })
+      
+          
+          
+      });
 }
 
+
+
+
+
+
+
+server.listen(3000, function(){
+  console.log('Server running at https://127.0.0.1:3000/');
+})
